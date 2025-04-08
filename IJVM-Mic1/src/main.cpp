b0f717b5852a1452ef55ddee96d73e3abb8be9db
Adriane Lopes <adrianelf04@gmail.com>
2025-04-07 23:23:38 -0300

    iniciando a main

--- IJVM-Mic1/src/main.cpp
+++ IJVM-Mic1/src/main.cpp
@@ -1,6 +1,84 @@
 #include "ijvm.h"
 #include <iostream>
+#include <fstream>
+#include <string>
+#include <bitset>
 
-int main(){
+using namespace std;
+
+int main()
+{
+
+    // Leitura de arquivos:
+    // Abrir os arquivos
+    ifstream entrada("programa_etapa1.txt");
+    ofstream saida("saída_etapa1.txt");
+
+    if (!entrada.is_open() || !saida.is_open())
+    {
+        std::cerr << "Erro ao abrir arquivos de entrada ou saída.\n";
+        return 1;
+    }
+
+    // Início do programa
+    saida << "b = " << std::bitset<32>(1) << "\n";
+    // Aqui usamos -1 para representar 0xFFFFFFFF (todos 1)
+    saida << "a = " << std::bitset<32>(static_cast<unsigned int>(-1)) << "\n\n";
+    saida << "Start of Program\n";
+    saida << "============================================================\n";
+
+    string linha;
+    int PC = 0;
+
+    /*     Instrução:  Esta palavra deve ser armazenada em uma variável que representa o registrador de
+    instrução (IR), e uma outra variável deve atuar como o contador de programa (PC),
+    definindo que linha do programa está sendo executada. */
+    while (getline(entrada, linha))
+    {
+
+        if (linha.empty())
+        {
+            saida << "Cycle " << (PC + 1) << "\n\n";
+            saida << "PC = " << (PC + 1) << "\n";
+            saida << "> Line is empty, EOP.\n";
+            break;
+        }
+
+        if (linha.length() != 6)
+        {
+            std::cerr << "Erro\n";
+            continue;
+        }
+        char IR[6];
+        std::copy(linha.begin(), linha.begin() + 6, IR);
+
+        // Chama a função da ULA
+        std::pair<unsigned int, unsigned int> resultado_Ula = ula6bits(IR);
+        int A = IR[2] - '0';
+        int B = IR[3] - '0';
+
+        // Pegar o valor que foi usado de A
+        if (IR[4] == '1')
+            A = !A;
+        // Aqui eu fiquei em dúvida se ele quer registrar o valor de A antes ou depois da inversão, me ajudem a decidir
+
+        int S = resultado_Ula.first;
+        int vai_um = resultado_Ula.second;
+
+        saida << "Cycle " << (PC + 1) << "\n\n";
+        saida << "PC = " << (PC + 1) << "\n";
+        saida << "IR = " << linha << "\n";
+        saida << "b = " << std::bitset<32>(1) << "\n";
+        saida << "a = " << std::bitset<32>(A ? static_cast<unsigned int>(-1) : 0) << "\n";
+        saida << "s = " << std::bitset<32>(S) << "\n";
+        saida << "co = " << vai_um << "\n";
+        saida << "============================================================\n";
+
+        PC++;
+    }
+
+    entrada.close();
+    saida.close();
+
     return 0;
 }
